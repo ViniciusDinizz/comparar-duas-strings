2351e21b1e12d443d5392d89ae7575a10a074b9b
ViniciusDinizz <viniciusdiniz.ti/2gmail.com>
2023-03-07 23:14:42 -0300

    Add project files.

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string primeira, segunda;
+
+        Console.Write("Primeira string: ");
+        primeira = Console.ReadLine();
+        Console.Write("Segunda string: ");
+        segunda = Console.ReadLine();
+
+        if(comparar(primeira, segunda))
+        {
+            Console.WriteLine("São iguais.");
+        }
+        else
+        {
+            Console.WriteLine("São diferentes.");
+        }
+
+        bool comparar(string primeira, string segunda)
+        {
+            if (primeira == segunda)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
